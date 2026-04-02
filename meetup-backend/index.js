f2e8418b8b078bfd637c5c7af67cf445c872a95f
Anupma-Puri <anupma.puri10@gmail.com>
2026-04-02 13:24:09 +0530

    Updated backend for deployment

--- meetup-backend/index.js
+++ meetup-backend/index.js
@@ -184,7 +184,8 @@ app.get("/events/:eventId", (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
